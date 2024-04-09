12b5510abe6a60cce0f6fb0e650ed53864caf935
Tae Hyeon Jo <hillskuti@naver.com>
2024-04-09 21:37:08 +0900

    Next Auth3 회원가입 구현

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,9 @@
 import { connectToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
 
 async function handler(req, res) {
+  if (req.method !== 'POST') return;
+
   const data = req.body;
 
   const { email, password } = data;
@@ -22,14 +25,15 @@ async function handler(req, res) {
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
-  const result = db.collections('users').insertOne({
+  const result = await db.collection('users').insertOne({
     email: email,
     password: hashedPassword,
   });
 
-  res.status(200).json({ meassage: 'Created user!' });
+  res.status(201).json({ message: 'Created user!' });
+  client.close();
 }
 
 export default handler;
